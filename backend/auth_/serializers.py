1c53b399cacb8125182661876bd23432df2ba1b3
Derbes <madikhanepiko@gmail.com>
2020-04-24 09:05:50 +0600

    profile

--- backend/auth_/serializers.py
+++ backend/auth_/serializers.py
@@ -2,26 +2,17 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
 
-
-
-
-
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'first_name', 'last_name')
-
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'first_name', 'last_name')
-
+        fields = ('id', 'username', 'password', 'email',)
 
 class ProfileSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     created_at = serializers.DateTimeField(required=False)
-    user = UserSerializer(required=True)
+    user = UserSerializer(required=False)
 
     class Meta:
         model = Profile
--- backend/auth_/urls.py
+++ backend/auth_/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from auth_.views import UserCreateView, login, me, ProfilesAPIView
+from auth_.views import UserCreateView, login, ProfilesAPIView
 
 
 urlpatterns = [
 
     path('login/', login),
-    # path('logout/', views.logout),
-    path('me/', me),
     path('register/', UserCreateView.as_view()),
-    path('profilecreate/', ProfilesAPIView.as_view()),
+    path('create/', ProfilesAPIView.as_view()),
 ]
